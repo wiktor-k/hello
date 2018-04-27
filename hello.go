064813f48cee437ecd61876605124fda3c09dbf0
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2018-04-27 15:34:33 +0200

    Add sample documentation

--- hello.go
+++ hello.go
@@ -1,3 +1,6 @@
+// Prints sample string serving as a demonstration that
+// go framework works as expected.
+// See: https://en.wikipedia.org/wiki/%22Hello,_World!%22_program
 package main // import "metacode.biz/hello"
 
 import "fmt"
